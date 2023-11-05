fa6c17a9d8403eed1e337bd7ea0b52f09f613af4
Gabriela Gonzales <gonzales.villarroel.gabriela@gmail.com>
2023-11-05 12:38:52 -0400

    Added navbar and actions for cart

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,42 @@
 import './App.css';
-import { Button, CircularProgress } from '@mui/material';
+import { useEffect } from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation
+} from 'react-router-dom';
+import Home from "./scenes/home/Home";
+import ItemDetails from './scenes/itemDetails/ItemDetails';
+import Checkout from './scenes/checkout/Checkout';
+import Confirmation from './scenes/checkout/Confirmation';
+import Navbar from './scenes/global/Navbar';
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname])
+
+  return null;
+}
 
 function App() {
   return (
     <>
-      <Button variant="contained">Contained</Button>
-      <Button variant="contained" disabled>
-        Disabled
-      </Button>
-      <Button variant="contained" href="#contained-buttons">
-        Link
-      </Button>
-      <CircularProgress />
+      <div className='app'>
+        <BrowserRouter>
+          <Navbar/>
+          <ScrollToTop/>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="item/:itemId" element={<ItemDetails />}/>
+            <Route path="checkout" element={<Checkout />}/>
+            <Route path="checkout/success" element={<Confirmation />}/>
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
   );
 }
